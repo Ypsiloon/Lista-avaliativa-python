c12a23ba2f443e2a2026251d91544504dc9557a3
Bipe <60533993+Ypsiloon@users.noreply.github.com>
2023-09-03 22:08:14 -0300

    ex2.py

--- ex2.py
+++ ex2.py
@@ -1,8 +1,29 @@
+# Jogo da Velha
+# Este é um jogo da velha para dois jogadores com um tabuleiro de tamanho variável.
+# O tabuleiro é uma matriz quadrada representada como uma lista de listas.
+# O jogo continua até que um jogador vença ou haja um empate.
+
 def criar_tabuleiro(tamanho):
+    """
+    Cria um tabuleiro vazio com o tamanho especificado.
+
+    Args:
+        tamanho: O tamanho do tabuleiro (número de linhas e colunas).
+
+    Retorna:
+        lista: O tabuleiro vazio como uma lista de listas.
+    """
     tabuleiro = [ [ '_' for i in range(tamanho)] for j in range(tamanho)]
     return tabuleiro
 
 def jogada(jogador_vez):
+    """
+    Solicita a coordenada para a jogada do jogador e atualiza o tabuleiro.
+
+    Args:
+        jogador_vez: O jogador atual ('X' ou 'O').
+
+    """
     while True:
         jogador = input("Digite a cordenada que deseja jogar [linha][coluna]: ")
         linha,coluna = [int(posicao) for posicao in jogador.split()]
@@ -15,10 +36,23 @@ def jogada(jogador_vez):
             break
     
 def desenhar_tabuleiro():
+    """
+    Desenha o tabuleiro na tela.
+    
+    """
     for i in jogo_da_velha:
         print(i)
 
 def variar_jogador(count_jogada):
+    """
+    Alterna entre os jogadores 'X' e 'O' com base no número de jogadas.
+
+    Args:
+        count_jogada: O número de jogadas até agora.
+
+    Retorna:
+        str: O jogador atual ('X' ou 'O').
+    """
     if count_jogada % 2 == 0:
         jogador_vez =  'O'
     else:
@@ -26,6 +60,12 @@ def variar_jogador(count_jogada):
     return jogador_vez
 
 def ganhou():
+    """
+    Verifica se um jogador venceu o jogo.
+
+    Retorna:
+        bool: True se algum jogador vencer, False caso contrário.
+    """
     for linha in jogo_da_velha:
         if all(item == linha[0] and item != "_" for item in linha):
             return True
